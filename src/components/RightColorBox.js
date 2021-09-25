90b765d13fdc47ebd9df93b4fea76156dae25769
VICTOR EJIKE NWOSU <74430629+Eprince-hub@users.noreply.github.com>
2021-09-25 12:43:42 +0200

    fix outdated package [preflight error]

--- src/components/RightColorBox.js
+++ src/components/RightColorBox.js
@@ -1,8 +1,3 @@
-import { prependOnceListener } from 'process';
-import React from 'react';
-import ColorValueDisplay from './ColorValueDisplay';
-import CustomBoxDisplay from './CustomBoxDisplay';
-
 function RightColorBox(props) {
   return (
     <div
